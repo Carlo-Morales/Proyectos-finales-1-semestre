43914cc3782f02d2604314c56f5d9b17ef72dab1
Carlo Ramses Morales Martinez <snarto@hotmail.com>
2025-11-10 10:44:43 -0600

    "Arreglando los archivos de conversión"

--- main.cpp
+++ main.cpp
@@ -3,12 +3,19 @@
 #include <vector>
 #include <algorithm>
 #include <math.h>
+#include "Conversiones/C_String_a_binario.h"
 
 using namespace std;
+string url;
+vector <int> ascii;
 
 int main(){
     cout<<"INTRODUCE UNA LIGA";
-
+    cin >> url;
+    for (int i=0; i<url.size(); i++){
+        int x =string_a_binario(url, ascii);
+        cout << x;
+    }
 
     return 0;
 }
