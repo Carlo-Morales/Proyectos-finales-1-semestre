c0847f339c33577963a1437ec5d5763f684505c1
Carlo Ramses Morales Martinez <snarto@hotmail.com>
2025-11-12 10:40:27 -0600

    "Arreglando elementos"

--- Application/Matrices/M_Elementos_fijos.h
+++ Application/Matrices/M_Elementos_fijos.h
@@ -1,7 +1,8 @@
+#ifndef M_ELEMENTOS_FIJOS_H
+#define M_ELEMENTOS_FIJOS_H
+
 #include <iostream>
 #include <vector>
-#include "M_Elementos_fijos.h"
-
 
 const int TAMANO_QR = 27;
 const int BLANCO = 0;
@@ -44,34 +45,34 @@ void dibujarBordeExterno(){
         }
     }
 }
-void dibujarFinderPattern(int FILA_INICIO, int COLUMNA_INCIO) {
+void dibujarFinderPattern(int fila_inicio, int columna_inicio) {
     for (int i = 0; i < 7; ++i) { 
         for (int j = 0; j < 7; ++j) { 
             if (i == 0 || i == 6 || j == 0 || j == 6 || (i > 1 && i < 5 && j > 1 && j < 5)) 
             {
-                matriz_qr[FILA_INICIO + i][COLUMNA_INCIO + j] = NEGRO; 
+                matriz_qr[fila_inicio + i][columna_inicio + j] = NEGRO; 
             } else {
-                matriz_qr[FILA_INICIO + i][COLUMNA_INCIO + j] = BLANCO; 
+                matriz_qr[fila_inicio + i][columna_inicio + j] = BLANCO; 
             }
         }
     }
 }
-void dibujarFinderPattern_2(int FILA_INICIO, int COLUMNA_INCIO){
-    for (int i=FILA_INICIO; i < 9; i++){
-            matriz_qr[FILA_INICIO + i][COLUMNA_INCIO] = BLANCO;
+void dibujarFinderPattern_2(int fila_inicio, int columna_inicio){
+    for (int i=fila_inicio; i < 9; i++){
+            matriz_qr[fila_inicio + i][columna_inicio] = BLANCO;
     }
-        for (int j=FILA_INICIO; j < 9; j++){
-            matriz_qr[COLUMNA_INCIO][FILA_INICIO + j] = BLANCO;
-    }
-}
-void dibujarFinderPattern_3(int FILA_INICIO, int COLUMNA_INCIO){
-    for (int j=COLUMNA_INCIO; j <= TAMANO_QR - 1; j++){
-            matriz_qr[FILA_INICIO][j] = BLANCO;
+        for (int j=fila_inicio; j < 9; j++){
+            matriz_qr[columna_inicio][fila_inicio + j] = BLANCO;
     }
 }
-void dibujarFinderPattern_4(int FILA_INICIO, int COLUMNA_INCIO){
-    for (int i=FILA_INICIO; i <= TAMANO_QR - 1; i++){
-            matriz_qr[i][COLUMNA_INCIO] = BLANCO;
+void dibujarFinderPattern_3(int fila_inicio, int columna_inicio){
+    for (int j=columna_inicio; j <= TAMANO_QR - 1; j++){
+            matriz_qr[fila_inicio][j] = BLANCO;
+    }
+}
+void dibujarFinderPattern_4(int fila_inicio, int columna_inicio){
+    for (int i=fila_inicio; i <= TAMANO_QR - 1; i++){
+            matriz_qr[i][columna_inicio] = BLANCO;
     }
 }
 void PatronDeTemporizador(){ 
@@ -107,6 +108,7 @@ void ElementoNegro() {
     matriz_qr[19][9] = NEGRO; 
 }
 void imprimirQR() {
+    cout << "\n";
     for (int i = 0; i < TAMANO_QR; ++i) {
         for (int j = 0; j < TAMANO_QR; ++j) {
             if (matriz_qr[i][j] == NEGRO) {
@@ -120,3 +122,5 @@ void imprimirQR() {
         cout << "\n";
     }
 }
+
+#endif
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <math.h>
 #include "Application/Conversiones/C_String_a_binario.h"
+#include "Application/Matrices/M_Elementos_fijos.h"
 
 using namespace std;
 string url;
@@ -16,10 +17,26 @@ vector <bitset<8>> binario;
 int main(){
     cout<<"INTRODUCE UNA LIGA\n";
     cin >> url;
+//STRING A BINARIO
     int x = string_a_binario(url, ascii, binario);//función mandada a llamar
     //Impresión de la liga en binario para fines prácticos
     for (int i=0; i<binario.size(); i++){
         cout << binario[i] << "  ";
     }
+//ELEMENTOS FIJOS
+    inicializarMatriz();
+    dibujarBordeExterno();
+    dibujarFinderPattern(1, 1); 
+    dibujarFinderPattern(1, 19);  
+    dibujarFinderPattern(19, 1); 
+    dibujarFinderPattern_2(0,8);
+    dibujarFinderPattern_2(0,18);
+    dibujarFinderPattern_2(18,0);
+    dibujarFinderPattern_3(8,19);
+    dibujarFinderPattern_4(19,8);
+    PatronDeTemporizador();
+    PatronDeAlineacion();
+    ElementoNegro();
+    imprimirQR();
     return 0;
 }
